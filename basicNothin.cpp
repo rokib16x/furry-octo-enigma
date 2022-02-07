44624751086f4c0120d31c577190c75e5698a5dc
Rokibul Hasan <86328558+rokib16x@users.noreply.github.com>
2022-02-08 03:03:43 +0600

    Create basicNothin.cpp

---
+++ basicNothin.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    string fiasco=  "Hello My Imaginary Friend, Fiasco,UK";
+    //               0123456789x123456789x123456789x12345
+//    cout<<fiasco[19]<<fiasco[34]<<fiasco[31]<<fiasco[35];
+//    cout<<fiasco.find("Fiasco",0)<<endl;
+//    cout<<fiasco.substr(27,6);
+//    cout<<10/3.0;
+    cout<<pow(5,2)<<endl;
+    cout<<sqrt(36)<<endl;
+    cout<<round(11.5)<<endl;
+    cout<<ceil(4.1)<<endl;
+    cout<<floor(2.6)<<endl;
+    cout<<fmax(30,31)<<endl;
+    cout<<fmin(43,12)<< endl;
+    return 0;
+}
