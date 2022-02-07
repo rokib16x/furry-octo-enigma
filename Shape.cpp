5239d0235693b9e8f5fae27484abdd32be6f5701
Rokibul Hasan <86328558+rokib16x@users.noreply.github.com>
2022-02-08 01:51:34 +0600

    Create Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"     /| "<<endl;
+    cout<<"    / |"<<endl;
+    cout<<"   /  |"<<endl;
+    cout<<"  /   |"<<endl;
+    cout<<" /    |"<<endl;
+    cout<<"/_____|"<<endl;
+    return 0;
+}
