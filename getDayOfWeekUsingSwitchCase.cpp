77bd0d30c18779203922dc7c32f81df2dc4f5898
Rokibul Hasan <86328558+rokib16x@users.noreply.github.com>
2022-02-13 03:22:52 +0600

    Create getDayOfWeekUsingSwitchCase.cpp

---
+++ getDayOfWeekUsingSwitchCase.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+string getDayOfWeek(int condition);
+
+string getDayOfWeek(int condition){
+    string dayName;
+
+    switch (condition) {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+            dayName = "Invalid Day Number";
+    }
+    return dayName;
+}
+
+
+int main() {
+    int x;
+    cin >>x;
+    cout<< "Your input number converted in to: "<<getDayOfWeek(x);
+    return 0;
+}
